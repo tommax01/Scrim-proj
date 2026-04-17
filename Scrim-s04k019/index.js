3209c00638256c0a1369dd16ababee73902281ac
tommax01 <159420882+tommax01@users.noreply.github.com>
2026-04-17 18:23:06 +0300

    Add files via upload

---
+++ Scrim-s04k019/index.js
@@ -0,0 +1,38 @@
+document.body.style.backgroundColor = "#111827";
+document.getElementById("C").style.color = "white";
+// document.getElementById("in").style.color = "red";
+// document.getElementById("in").style.fontSize = "50px";
+// document.getElementById("in").style.paddingLeft = "15px";
+let inp = document.getElementById("in")
+let inn = document.getElementById("inn")
+count1 = 0
+count2 = 0
+
+// inp.value = 0
+
+function chaneNum1(){
+    count1 += 1
+    inp.value = count1;
+}
+function chaneNum2(){
+    count1 += 2
+    inp.value = count1;
+}
+function chaneNum3(){
+    count1 += 3
+    inp.value = count1;
+}
+
+
+function chaneNum4(){
+    count2 += 1
+    inn.value = count2;
+}
+function chaneNum5(){
+    count2 += 2
+    inn.value = count2;
+}
+function chaneNum6(){
+    count2 += 3
+    inn.value = count2;
+}
